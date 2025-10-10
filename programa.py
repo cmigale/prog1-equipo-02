28e32f84ae47b9f7bd68f8f41c3b889899aaf4aa
Camila Migale <cmigale@uade.edu.ar>
2025-10-10 19:30:43 -0300

    Update programa.py

--- programa.py
+++ programa.py
@@ -1,28 +1,26 @@
 import random
-stock=[["Producto:","Codigo:","Cantidad en stock:","Precio Unitario:",]]
+stock=[]
 def añadirProducto(x,y,w):
     z=random.randint(100000,999999) #Genero el codigo del producto
     repetido=0
     while repetido: #Verifico que el codigo no exista previamente
         repetido=0
-        for fila in stock:
-            if z==fila[3]:
+        for producto in stock:
+            if producto["codigo"] == z:
                 repetido=1
                 z=random.randint(100000,999999)
-    producto=[x,z,y,w]
+    producto={"codigo":z ,"producto":x , "cantidad en stock":y, "Precio Unitario":w}
     stock.append(producto)
-    print(x,"añadido al stock con exito!")
+    print(f"¡{x} añadido al stock con éxito!")
 def buscar_valor_matriz(valor, stock):
-    print(stock)
     ubicacion = -1
     valor = str(valor).lower()
-    for i in range(len(stock[0])):  # recorro por columnas
-        if valor == str(stock[0][i]).lower() or valor == str(stock[1][i]).lower():
+    for i in range(len(stock)): #recorro cada producto 
+        if valor == str(stock[i]["producto"]).lower():
             ubicacion = i + 1  # guardo la posición (1-based)
     return ubicacion
 
 
-print(stock)
 ifagregar=int(input("Ingrese un 1 si quiere agregar un producto, 0 en caso contrario:"))
 while ifagregar!=1 and ifagregar!=0:
     ifagregar=int(input("Ingrese un 1 si quiere agregar un producto, 0 en caso contrario:"))
@@ -32,8 +30,20 @@ if ifagregar==1:
         if len(stock)>1:
             if buscar_valor_matriz(aprod, stock)!=-1:
                 aprod=input("El producto ya se encuentra en la matriz, ingrese otro producto:")
-        acant=int(input("Ingrese la cantidad disponible del producto:"))
-        aprecio=int(input("Ingrese el precio unitario del producto:"))
+        valido=False
+        while valido==False:
+            try:
+                acant=int(input("Ingrese la cantidad disponible del producto:"))
+                valido=True
+            except ValueError:
+                print("Entrada invalida. No introdujiste un número entero.")
+        valido=False
+        while valido==False:
+            try:
+                aprecio=float(input("Ingrese el precio unitario del producto:"))
+                valido=True
+            except ValueError:
+                print("Entrada invalida. No introdujiste un número.")
         añadirProducto(aprod,acant,aprecio)
         ifagregar=int(input("Ingrese un 1 si quiere agregar un producto, 0 en caso contrario:"))
         while ifagregar!=1 and ifagregar!=0:
@@ -41,3 +51,4 @@ if ifagregar==1:
 print("------Stock Actual------")
 for fila in stock:
     print(fila)
+
